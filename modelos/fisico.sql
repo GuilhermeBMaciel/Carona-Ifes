fd1c741f7ba5bbd49b9acfd9afb4cf8b30fc90b4
Guilherme Maciel <guilhermemaciel2@hotmail.com>
2018-04-06 14:44:30 -0300

    Update fisico.sql

--- modelos/fisico.sql
+++ modelos/fisico.sql
@@ -8,22 +8,26 @@ CREATE TABLE Usuario (
     email VARCHAR,
     modo_motorista BOOLEAN,
     senha VARCHAR,
-    cnh VARCHAR
+    cnh VARCHAR,
+    Usuario_TIPO INT
 );
 
-CREATE TABLE Itinerario (
+CREATE TABLE carona (
+    vaga_motorista VARCHAR,
     vaga_usuario INTEGER,
     id INTEGER PRIMARY KEY,
-    tempo_ativo TIME,
-    FK_local_id VARCHAR
+    tempo_ativo INTEGER,
+    avaliacao INTEGER,
+    nota_avaliacao INTEGER,
+    FK_local_id INTEGER
 );
 
 CREATE TABLE local (
     local_de_saida VARCHAR,
     horario_saida TIME,
     local_de_retorno VARCHAR,
-    horario_retorno TIME,
-    id VARCHAR PRIMARY KEY
+    horario_retorno VARCHAR,
+    id INTEGER PRIMARY KEY
 );
 
 CREATE TABLE veiculo (
@@ -33,65 +37,37 @@ CREATE TABLE veiculo (
     id INTEGER PRIMARY KEY
 );
 
-CREATE TABLE historico (
-    nota INTEGER,
-    descricao VARCHAR,
-    id INTEGER PRIMARY KEY,
-    nome_usuario VARCHAR
-);
-
-CREATE TABLE Registra (
-    FK_Itinerario_id INTEGER,
+CREATE TABLE possui (
+    FK_carona_id INTEGER,
     FK_Usuario_login VARCHAR
 );
 
-CREATE TABLE Busca (
-    FK_Itinerario_id INTEGER,
-    FK_Usuario_login VARCHAR
-);
-
-CREATE TABLE possui_veiculo_Usuario_historico (
+CREATE TABLE possui (
     FK_veiculo_id INTEGER,
-    FK_Usuario_login VARCHAR,
-    FK_historico_id INTEGER
+    FK_Usuario_login VARCHAR
 );
  
-ALTER TABLE Itinerario ADD CONSTRAINT FK_Itinerario_1
+ALTER TABLE carona ADD CONSTRAINT FK_carona_1
     FOREIGN KEY (FK_local_id)
     REFERENCES local (id)
     ON DELETE CASCADE ON UPDATE CASCADE;
  
-ALTER TABLE Registra ADD CONSTRAINT FK_Registra_0
-    FOREIGN KEY (FK_Itinerario_id)
-    REFERENCES Itinerario (id)
+ALTER TABLE possui ADD CONSTRAINT FK_possui_0
+    FOREIGN KEY (FK_carona_id)
+    REFERENCES carona (id)
     ON DELETE SET NULL ON UPDATE CASCADE;
  
-ALTER TABLE Registra ADD CONSTRAINT FK_Registra_1
+ALTER TABLE possui ADD CONSTRAINT FK_possui_1
     FOREIGN KEY (FK_Usuario_login)
     REFERENCES Usuario (login)
     ON DELETE SET NULL ON UPDATE CASCADE;
  
-ALTER TABLE Busca ADD CONSTRAINT FK_Busca_0
-    FOREIGN KEY (FK_Itinerario_id)
-    REFERENCES Itinerario (id)
-    ON DELETE SET NULL ON UPDATE CASCADE;
- 
-ALTER TABLE Busca ADD CONSTRAINT FK_Busca_1
-    FOREIGN KEY (FK_Usuario_login)
-    REFERENCES Usuario (login)
-    ON DELETE SET NULL ON UPDATE CASCADE;
- 
-ALTER TABLE possui_veiculo_Usuario_historico ADD CONSTRAINT FK_possui_veiculo_Usuario_historico_0
+ALTER TABLE possui ADD CONSTRAINT FK_possui_0
     FOREIGN KEY (FK_veiculo_id)
     REFERENCES veiculo (id)
-    ON DELETE NO ACTION ON UPDATE NO ACTION;
+    ON DELETE SET NULL ON UPDATE CASCADE;
  
-ALTER TABLE possui_veiculo_Usuario_historico ADD CONSTRAINT FK_possui_veiculo_Usuario_historico_1
+ALTER TABLE possui ADD CONSTRAINT FK_possui_1
     FOREIGN KEY (FK_Usuario_login)
     REFERENCES Usuario (login)
-    ON DELETE NO ACTION ON UPDATE NO ACTION;
- 
-ALTER TABLE possui_veiculo_Usuario_historico ADD CONSTRAINT FK_possui_veiculo_Usuario_historico_2
-    FOREIGN KEY (FK_historico_id)
-    REFERENCES historico (id)
-    ON DELETE NO ACTION ON UPDATE NO ACTION;
+    ON DELETE SET NULL ON UPDATE CASCADE;
