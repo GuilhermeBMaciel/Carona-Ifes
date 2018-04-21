3ad26b3b286ae09557d1aacd35d94fdc04df133c
Guilherme Maciel <guilhermemaciel2@hotmail.com>
2018-04-21 15:58:43 -0300

    Update create-insert.sql

--- arquivosSQL/create-insert.sql
+++ arquivosSQL/create-insert.sql
@@ -1,4 +1,5 @@
 /* Creates: */
+/* Lógico_1: */
 
 CREATE TABLE Usuario (
     login VARCHAR PRIMARY KEY,
@@ -18,10 +19,9 @@ CREATE TABLE carona (
 );
 
 CREATE TABLE localizacao (
-    horario_saida TIME,
-    horario_chegada TIME,
-    id SERIAL PRIMARY KEY,
-    FK_cep_cep VARCHAR
+    horario_saida VARCHAR,
+    horario_chegada VARCHAR,
+    id SERIAL PRIMARY KEY
 );
 
 CREATE TABLE veiculo (
@@ -48,7 +48,7 @@ CREATE TABLE consulta (
     FK_Usuario_login VARCHAR
 );
 
-CREATE TABLE possui (
+CREATE TABLE tem (
     FK_veiculo_id SERIAL,
     FK_motorista_FK_Usuario_login VARCHAR
 );
@@ -57,15 +57,15 @@ CREATE TABLE registra (
     FK_carona_id SERIAL,
     FK_motorista_FK_Usuario_login VARCHAR
 );
+
+CREATE TABLE possui (
+    FK_cep_cep VARCHAR,
+    FK_localizacao_id SERIAL
+);
  
 ALTER TABLE carona ADD CONSTRAINT FK_carona_1
     FOREIGN KEY (FK_localizacao_id)
     REFERENCES localizacao (id)
-    ON DELETE CASCADE ON UPDATE CASCADE;
- 
-ALTER TABLE localizacao ADD CONSTRAINT FK_localizacao_1
-    FOREIGN KEY (FK_cep_cep)
-    REFERENCES cep (cep)
     ON DELETE CASCADE ON UPDATE CASCADE;
  
 ALTER TABLE motorista ADD CONSTRAINT FK_motorista_1
@@ -83,12 +83,12 @@ ALTER TABLE consulta ADD CONSTRAINT FK_consulta_1
     REFERENCES Usuario (login)
     ON DELETE SET NULL ON UPDATE CASCADE;
  
-ALTER TABLE possui ADD CONSTRAINT FK_possui_0
+ALTER TABLE tem ADD CONSTRAINT FK_tem_0
     FOREIGN KEY (FK_veiculo_id)
     REFERENCES veiculo (id)
     ON DELETE SET NULL ON UPDATE CASCADE;
  
-ALTER TABLE possui ADD CONSTRAINT FK_possui_1
+ALTER TABLE tem ADD CONSTRAINT FK_tem_1
     FOREIGN KEY (FK_motorista_FK_Usuario_login)
     REFERENCES motorista (FK_Usuario_login)
     ON DELETE SET NULL ON UPDATE CASCADE;
@@ -102,11 +102,23 @@ ALTER TABLE registra ADD CONSTRAINT FK_registra_1
     FOREIGN KEY (FK_motorista_FK_Usuario_login)
     REFERENCES motorista (FK_Usuario_login)
     ON DELETE SET NULL ON UPDATE CASCADE;
+ 
+ALTER TABLE possui ADD CONSTRAINT FK_possui_0
+    FOREIGN KEY (FK_cep_cep)
+    REFERENCES cep (cep)
+    ON DELETE SET NULL ON UPDATE CASCADE;
+ 
+ALTER TABLE possui ADD CONSTRAINT FK_possui_1
+    FOREIGN KEY (FK_localizacao_id)
+    REFERENCES localizacao (id)
+    ON DELETE SET NULL ON UPDATE CASCADE;
     
     /* Inserts */
+    
 truncate carona restart identity cascade;
 truncate veiculo restart identity cascade;
 truncate localizacao restart identity cascade;
+
 INSERT INTO usuario (login, nome, matricula_ou_siape, telefone, email, senha)
 VALUES  ('luiz12', 'Luiz Gonçalves', '20181bsi0178', '(27)99626375','luizgoncalves@gmail.com', 'password'),
 ('carlos1', 'Carlos Patricio', '20181bsi0179', '(27)88626375', 'carlospatricio@gmail.com', 'password1'),
@@ -117,20 +129,39 @@ VALUES  ('luiz12', 'Luiz Gonçalves', '20181bsi0178', '(27)99626375','luizgoncal
 ('furto', 'Marcela Furtado', '20181bsi0184', '(27)91626375', 'marcelafurtado@gmail.com', 'password6'),
 ('pedro12', 'Pedro Paulo', '20181bsi0185', '(27)94326375', 'pedropaulo@gmail.com', 'password7'),
 ('ciare', 'Luciano Pereira', '20181bsi0186', '(27)99644375', 'lucianopereira@gmail.com', 'password8'),
-('calids', 'Carolina Dias', '20181bsi0187', '(27)99666676', 'carolinadias@gmail.com', 'password9');
-('lssa', 'Lucas Santana', '20181bsi0188', '(27)99666677', 'lucasantana@gmail.com', 'password10');
-('pecalo', 'Pedro Carvalho', '20181bsi0189', '(27)99666678', 'pedrocarvalho@gmail.com', 'password11');
-('hecaso', 'Henrique Cardoso', '20181bsi0190', '(27)99666679', 'henriquecardoso@gmail.com', 'password12');
-('mago', 'Marcela Gomes', '20181bsi0191', '(27)99666680', 'marcelagomes@gmail.com', 'password13');
-('dummo', 'Luiz Dummont', '20181bsi0192', '(27)99666681', 'luizdummont@gmail.com', 'password14');
-('valmeida', 'Valdenir Almeida', '20181bsi0193', '(27)99666682', 'valdeniralmeida@gmail.com', 'password15');
-('silva31', 'Carol Silva', '20181bsi0194', '(27)99666683', 'carolsilva@gmail.com', 'password16');
-('furbi', 'Luiza Furbino', '20181bsi0195', '(27)99666684', 'luizafurbino@gmail.com', 'password17');
-('ana3', 'Ana Souza', '20181bsi0196', '(27)99666685', 'anasouza@gmail.com', 'password18');
+('calids', 'Carolina Dias', '20181bsi0187', '(27)99666676', 'carolinadias@gmail.com', 'password9'),
+('lssa', 'Lucas Santana', '20181bsi0188', '(27)99666677', 'lucasantana@gmail.com', 'password10'),
+('pecalo', 'Pedro Carvalho', '20181bsi0189', '(27)99666678', 'pedrocarvalho@gmail.com', 'password11'),
+('hecaso', 'Henrique Cardoso', '20181bsi0190', '(27)99666679', 'henriquecardoso@gmail.com', 'password12'),
+('mago', 'Marcela Gomes', '20181bsi0191', '(27)99666680', 'marcelagomes@gmail.com', 'password13'),
+('dummo', 'Luiz Dummont', '20181bsi0192', '(27)99666681', 'luizdummont@gmail.com', 'password14'),
+('valmeida', 'Valdenir Almeida', '20181bsi0193', '(27)99666682', 'valdeniralmeida@gmail.com', 'password15'),
+('silva31', 'Carol Silva', '20181bsi0194', '(27)99666683', 'carolsilva@gmail.com', 'password16'),
+('furbi', 'Luiza Furbino', '20181bsi0195', '(27)99666684', 'luizafurbino@gmail.com', 'password17'),
+('ana3', 'Ana Souza', '20181bsi0196', '(27)99666685', 'anasouza@gmail.com', 'password18'),
 ('berta', 'Roberta Cruz', '20181bsi0197', '(27)99666686', 'robertacruz@gmail.com', 'password19');
 
-INSERT INTO cep (cep_saida, cep_chegada) VALUES
-();
+INSERT INTO cep (cep,municipio,bairro,rua) VALUES
+('29173-087','Serra','Manguinhos','Rodovia ES-010'),
+('29025-220','Vitoria','Vila Rubim','Avenida Duarte Lemos'),
+('29070-440','Vitoria','Maria Ortiz','Avenida Professor Fernando Duarte Rabelo'),
+('29045-300','Vitoria','Santa Luiza','Rua José Farias'),
+('29166-200','Serra','Barcelona','Avenida Região Sudeste'),
+('29045-402','Vitoria','Santa Luiza','Avenida Nossa Senhora da Penha'),
+('29172-105','Serra','Feu Rosa','Rua dos Cravos'),
+('29045-460','Vitoria','Consolação','Avenida Marechal Campos'),
+('29122-041','Vilha Velha','Glória','Rua Santa Terezinha'),
+('29168-600','Serra','Porto Canoa','Avenida Brasília'),
+('29164-050','Serra','Jardim Limoeiro','Avenida Lourival Nunes'),
+('29055-260','Vitoria','Praia do canto','Rua Aleixo Netto'),
+('29060-500','Vitoria','Jardim da penha','Rua Darcy Grijó'),
+('29043-405','Vitória','Tabuazeiro','Avenida Coronel José Martins de Figueiredo'),
+('29172-015','Serra','Feu Rosa','Rua das Avencas'),
+('29171-237','Serra','Serra Dourada II','Avenida Brasília'),
+('29122-030','Vila Velha','Glória','Avenida Getúlio Vargas'),
+('29106-595','Vila Velha','Cristovão Colombo','Rua Cristovão Colombo'),
+('29152-427','Cariacica','Tucum','Rua Anselmo Lage'),
+('29150-340','Cariacica','Itacibá','Rua Anselmo Modenesi');
 
 INSERT INTO motorista (cnh, fk_usuario_login) VALUES
 ('59089813750', 'luiz12'),
@@ -144,8 +175,39 @@ INSERT INTO motorista (cnh, fk_usuario_login) VALUES
 ('16437512555', 'dummo'),
 ('65988821658', 'furto');
 
-INSERT INTO localizacao (cep_saida, horario_chegada, horario_saida, cep_chegada) VALUES 
-();
+INSERT INTO localizacao (horario_saida, horario_chegada) VALUES 
+('13:00','14:00'),
+('7:30','13:00'),
+('08:00','14:00'),
+('14:00','16:00'),
+('13:15','19:00'),
+('13:20','20:00'),
+('08:00','22:00'),
+('09:00','12:00'),
+('10:20','13:00'),
+('12:00','19:00');
+
+INSERT INTO possui (fk_cep_cep, fk_localizacao_id) VALUES
+('29173-087', '1'),
+('29025-220', '1'),
+('29173-087', '2'),
+('29070-440', '2'),
+('29173-087', '3'),
+('29045-300', '3'),
+('29173-087', '4'),
+('29166-200', '4'),
+('29173-087', '5'),
+('29045-402', '5'),
+('29173-087', '6'),
+('29172-105', '6'),
+('29173-087', '7'),
+('29045-460', '7'),
+('29173-087', '8'),
+('29122-041', '8'),
+('29173-087', '9'),
+('29168-600', '9'),
+('29164-050', '10'),
+('29173-087', '10');
 
 INSERT INTO carona (avaliacao, nota_avaliacao, qtd_vagas, fk_localizacao_id) VALUES 
 ('bom motorista', 4, 3, 1),
