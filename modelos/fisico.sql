693b0da705ee26a96945ddab5ef6940c417d3c9f
Guilherme Maciel <guilhermemaciel2@hotmail.com>
2018-04-24 10:49:08 -0300

    Update fisico.sql

--- modelos/fisico.sql
+++ modelos/fisico.sql
@@ -8,24 +8,26 @@ CREATE TABLE Usuario (
 );
 
 CREATE TABLE carona (
-    id SERIAL PRIMARY KEY,
+    id_carona SERIAL PRIMARY KEY,
     avaliacao VARCHAR,
     nota_avaliacao INTEGER,
     qtd_vagas INTEGER,
-    FK_localizacao_id SERIAL
+    FK_localizacao_id_localizacao SERIAL
 );
 
 CREATE TABLE localizacao (
-    horario_saida VARCHAR,
-    horario_chegada VARCHAR,
-    id SERIAL PRIMARY KEY
+    horario_saida TIME,
+    horario_chegada TIME,
+    id_localizacao SERIAL PRIMARY KEY,
+    FK_cep_cep_saida VARCHAR,
+    FK_cep_cep_chegada VARCHAR
 );
 
 CREATE TABLE veiculo (
     marca VARCHAR,
     modelo VARCHAR,
     cor VARCHAR,
-    id SERIAL PRIMARY KEY
+    id_veiculo SERIAL PRIMARY KEY
 );
 
 CREATE TABLE motorista (
@@ -41,38 +43,43 @@ CREATE TABLE cep (
 );
 
 CREATE TABLE consulta (
-    FK_carona_id SERIAL,
+    FK_carona_id_carona SERIAL,
     FK_Usuario_login VARCHAR
 );
 
 CREATE TABLE tem (
-    FK_veiculo_id SERIAL,
+    FK_veiculo_id_veiculo SERIAL,
     FK_motorista_FK_Usuario_login VARCHAR
 );
 
 CREATE TABLE registra (
-    FK_carona_id SERIAL,
+    FK_carona_id_carona SERIAL,
     FK_motorista_FK_Usuario_login VARCHAR
-);
-
-CREATE TABLE possui (
-    FK_cep_cep VARCHAR,
-    FK_localizacao_id SERIAL
 );
  
 ALTER TABLE carona ADD CONSTRAINT FK_carona_1
-    FOREIGN KEY (FK_localizacao_id)
-    REFERENCES localizacao (id)
+    FOREIGN KEY (FK_localizacao_id_localizacao)
+    REFERENCES localizacao (id_localizacao)
     ON DELETE CASCADE ON UPDATE CASCADE;
  
+ALTER TABLE localizacao ADD CONSTRAINT FK_localizacao_1
+    FOREIGN KEY (FK_cep_cep_saida)
+    REFERENCES cep ( cep)
+    ON DELETE CASCADE ON UPDATE CASCADE;
+    
+ALTER TABLE localizacao ADD CONSTRAINT FK_localizacao_2
+    FOREIGN KEY (FK_cep_cep_saida)
+    REFERENCES cep (cep)
+    ON DELETE CASCADE ON UPDATE CASCADE;
+
 ALTER TABLE motorista ADD CONSTRAINT FK_motorista_1
     FOREIGN KEY (FK_Usuario_login)
     REFERENCES Usuario (login)
     ON DELETE CASCADE ON UPDATE CASCADE;
  
 ALTER TABLE consulta ADD CONSTRAINT FK_consulta_0
-    FOREIGN KEY (FK_carona_id)
-    REFERENCES carona (id)
+    FOREIGN KEY (FK_carona_id_carona)
+    REFERENCES carona (id_carona)
     ON DELETE SET NULL ON UPDATE CASCADE;
  
 ALTER TABLE consulta ADD CONSTRAINT FK_consulta_1
@@ -81,8 +88,8 @@ ALTER TABLE consulta ADD CONSTRAINT FK_consulta_1
     ON DELETE SET NULL ON UPDATE CASCADE;
  
 ALTER TABLE tem ADD CONSTRAINT FK_tem_0
-    FOREIGN KEY (FK_veiculo_id)
-    REFERENCES veiculo (id)
+    FOREIGN KEY (FK_veiculo_id_veiculo)
+    REFERENCES veiculo (id_veiculo)
     ON DELETE SET NULL ON UPDATE CASCADE;
  
 ALTER TABLE tem ADD CONSTRAINT FK_tem_1
@@ -91,21 +98,11 @@ ALTER TABLE tem ADD CONSTRAINT FK_tem_1
     ON DELETE SET NULL ON UPDATE CASCADE;
  
 ALTER TABLE registra ADD CONSTRAINT FK_registra_0
-    FOREIGN KEY (FK_carona_id)
-    REFERENCES carona (id)
+    FOREIGN KEY (FK_carona_id_carona)
+    REFERENCES carona (id_carona)
     ON DELETE SET NULL ON UPDATE CASCADE;
  
 ALTER TABLE registra ADD CONSTRAINT FK_registra_1
     FOREIGN KEY (FK_motorista_FK_Usuario_login)
     REFERENCES motorista (FK_Usuario_login)
-    ON DELETE SET NULL ON UPDATE CASCADE;
- 
-ALTER TABLE possui ADD CONSTRAINT FK_possui_0
-    FOREIGN KEY (FK_cep_cep)
-    REFERENCES cep (cep)
-    ON DELETE SET NULL ON UPDATE CASCADE;
- 
-ALTER TABLE possui ADD CONSTRAINT FK_possui_1
-    FOREIGN KEY (FK_localizacao_id)
-    REFERENCES localizacao (id)
     ON DELETE SET NULL ON UPDATE CASCADE;
