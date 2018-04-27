216e2dffbc2ee10411116500fb70edbe1c3979cf
Guilherme Maciel <guilhermemaciel2@hotmail.com>
2018-04-27 10:05:35 -0300

    Update b.sql

--- Bloco 03/b.sql
+++ Bloco 03/b.sql
@@ -1,1 +1,9 @@
+create view local_hora_carona_saida as select localizacao.horario_saida as "Horario de saida",
+cep.cep as "Cep de saída", cep.municipio as "Município de saída", 
+cep.bairro as "Bairro de saída", cep.rua as "Rua de saída"from localizacao 
+inner join cep on (localizacao.fk_cep_cep_saida = cep.cep)
+order by localizacao.horario_saida;
 
+/* Printando */
+ 
+select * from local_hora_carona_saida;
