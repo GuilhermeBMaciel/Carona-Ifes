bfad47aab241764d06c5b90a5b2ffb90e5bc290c
Guilherme Maciel <guilhermemaciel2@hotmail.com>
2018-05-08 10:20:54 -0300

    Update b.sql

--- Bloco 03/b.sql
+++ Bloco 03/b.sql
@@ -1,6 +1,6 @@
 
 Create view destino as
-Select fk_cep_cep_chegada,COUNT(fk_cep_cep_chegada) as FreqChegada
+Select fk_cep_cep_chegada as Destino ,COUNT(fk_cep_cep_chegada) as Frequencia
 from localizacao
 group by fk_cep_cep_chegada
-order by FreqChegada desc;
+order by Frequencia desc;
