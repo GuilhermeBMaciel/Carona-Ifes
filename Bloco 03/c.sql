82147bc3024cffb3388998cbc4c3664e225d628f
jadsonpp <jadson_pereira5@hotmail.com>
2018-05-08 10:55:58 -0300

    Update c.sql

--- Bloco 03/c.sql
+++ Bloco 03/c.sql
@@ -1,1 +1,4 @@
-
+''' Checar condições no banco utilizando o operador "Exists" ou "not exists"
+EXISTS - verificar os motoristas que tem boas avaliações
+NOT EXIST - Verificar os motoristas que não tem avaliação
+'''
