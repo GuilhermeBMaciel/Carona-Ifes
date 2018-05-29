98ce2d7e08e88291c5d586ccdaba0d3b756ac031
jadsonpp <jadson_pereira5@hotmail.com>
2018-05-29 10:25:05 -0300

    Update b.sql

--- Bloco 03/b.sql
+++ Bloco 03/b.sql
@@ -1,6 +1,5 @@
-
 Create view destino as
-Select fk_cep_cep_chegada as Destino ,COUNT(fk_cep_cep_chegada) as Frequencia
+Select fk_cep_cep_ as Destino ,COUNT(fk_cep_cep_) as Frequencia
 from localizacao
-group by fk_cep_cep_chegada
+group by fk_cep_cep_
 order by Frequencia desc;
