9b4781dddee0aec168d4925fd55c1ed747934886
Guilherme Maciel <guilhermemaciel2@hotmail.com>
2018-06-08 11:17:28 -0300

    Update a.sql

--- Bloco 03/a.sql
+++ Bloco 03/a.sql
@@ -1,1 +1,15 @@
 
+/*inner join:*/
+
+select 	motorista.fk_usuario_login as "Motorista", carona.id_carona as "carona", carona.avaliacao as "Avaliacao", carona.nota_avaliacao as "Nota"
+from motorista inner join carona on (motorista.fk_usuario_login = carona.fk_motorista_fk_usuario_login);
+
+
+/*right join:*/
+
+
+
+/*left join:*/
+
+
+
