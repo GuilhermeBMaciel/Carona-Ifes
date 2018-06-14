022c691663abcc5f5347a0bb869b076c4d79c4ef
jadsonpp <jadson_pereira5@hotmail.com>
2018-06-14 16:38:24 -0300

    Update c.sql

--- Bloco 03/c.sql
+++ Bloco 03/c.sql
@@ -1,4 +1,7 @@
 ''' Checar condições no banco utilizando o operador "Exists" ou "not exists"
 EXISTS - verificar os motoristas que tem boas avaliações
+select exists (select * from carona c where avaliacao = 'otimo')
+
 NOT EXIST - Verificar os motoristas que não tem avaliação
+select exists (select * from carona c where avaliacao = 'horrivel')
 '''
