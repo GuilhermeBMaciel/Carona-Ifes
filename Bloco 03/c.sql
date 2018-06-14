e1cfcb4eb893b55d4cdbe48fe18d2ec3d019a5c2
jadsonpp <jadson_pereira5@hotmail.com>
2018-06-14 16:48:14 -0300

    Update c.sql

--- Bloco 03/c.sql
+++ Bloco 03/c.sql
@@ -4,4 +4,8 @@ select exists (select * from carona c where avaliacao = 'otimo')
 
 NOT EXIST - Verificar os motoristas que não tem avaliação
 select exists (select * from carona c where avaliacao = 'horrivel')
+
+se não existe carona acima da media.
+select not exists (select nota_avaliacao from carona c where nota_avaliacao = 10)
+
 '''
